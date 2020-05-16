59f71d263df94aebd26de693bb6297f219ef6ef6
ilseea <ame08004@byui.edu>
2020-05-15 20:43:52 -0400

    nav fixed

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -3,4 +3,4 @@ let date = today.getFullYear();
 document.getElementById('currentYr').innerHTML = date;
 
 let dateModified = document.lastModified;
-document.getElementById('datetime').innerHTML = dateModified
+document.getElementById('datetime').innerHTML = dateModified;
