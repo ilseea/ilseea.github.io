ee1b508b5d72acd996cc1d62b7d262a2e96e187b
ilseea <ame08004@byui.edu>
2020-05-17 01:03:56 -0400

    dateDONE

--- lesson5/js/date.js
+++ lesson5/js/date.js
@@ -1,6 +1,9 @@
 let today = new Date();
-let date = today.getFullYear();
-document.getElementById('currentYr').innerHTML = date;
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// document.getElementById("demo").innerHTML = days[d.getDay()];
 
-let dateModified = document.lastModified;
-document.getElementById('datetime').innerHTML = dateModified
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// document.getElementById("month").innerHTML = months[d.getMonth()];
+
+let date = days[today.getDay()] + ', ' + today.getDate() + " " + months[today.getMonth()] + " " + today.getFullYear();
+document.getElementById("dailydate").innerHTML = date;
