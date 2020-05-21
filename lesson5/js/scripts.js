0ec4cbfa914e3aa9cb4d7538eaf63180f6922de6
ilseea <ame08004@byui.edu>
2020-05-21 02:04:48 -0400

    gnight

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,17 @@
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+    document.querySelector(".navigation").classList.toggle("responsive");
+}
+
+
+
+
+
+
+
+
+
+/*    console.log(document.getElementById("primaryNav").classList);
+ */
