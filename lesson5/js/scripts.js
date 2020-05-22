1f4ed43e55d20ca28acb04ac811ee15e95c34c33
ilseea <ame08004@byui.edu>
2020-05-22 16:24:11 -0400

    try to commit again

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -2,8 +2,10 @@ const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
-    document.querySelector(".navigation").classList.toggle("responsive");
+  document.querySelector(".navigation").classList.toggle("responsive");
 }
+
+
 
 
 
