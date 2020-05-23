7e72ec23546a4fa1336f4fbf7d94c0684fc29980
ilseea <ame08004@byui.edu>
2020-05-23 12:11:34 -0400

    current

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,25 +1,35 @@
+//FOOTER DATE
+let today = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// document.getElementById("demo").innerHTML = days[d.getDay()];
+
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// document.getElementById("month").innerHTML = months[d.getMonth()];
+
+let date = days[today.getDay()] + ', ' + today.getDate() + " " + months[today.getMonth()] + " " + today.getFullYear();
+document.getElementById("dailydate").innerHTML = date;
+
+
+//NAVIGATION HAM BUTTON
+
 const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
-myFunction();
 
 function toggleMenu() {
   document.querySelector(".navigation").classList.toggle("responsive");
 }
 
-
+//BANNER ON FRIDAYS
 
 
 function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (friday... === "none") {
-    x.style.display = "block";
+  let today = new Date();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let date = days[today.getDay()] + ", " + today.getDate();
+  let banner = document.getElementById("myDIV").innerHTML;
+  if (date === "Saturday") {
+    banner.style.display = "none";
   } else {
-    x.style.display = "none";
+    banner.style.display = "none";
   }
 }
-
-
-
-
-/*    console.log(document.getElementById("primaryNav").classList);
- */
