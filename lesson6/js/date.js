19c3e062b33d2e841940ca8e4a8d18434b103c96
ilseea <ame08004@byui.edu>
2020-05-29 16:44:55 -0400

    save for now

---
+++ lesson6/js/date.js
@@ -0,0 +1,24 @@
+// let today = new Date();
+// let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// // document.getElementById("demo").innerHTML = days[d.getDay()];
+
+// let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// // document.getElementById("month").innerHTML = months[d.getMonth()];
+
+// let date = days[today.getDay()] + ', ' + today.getDate() + " " + months[today.getMonth()] + " " + today.getFullYear();
+// document.getElementById("dailydate").innerHTML = date;
+
+
+
+
+
+function hi() {
+
+    var banner = new Date().getDay();
+    if (banner === 6) {
+        pancakes.style.display = "none";
+    } else {
+        pancakes.style.display = "none";
+    }
+    document.getElementById("myDIV");
+}
