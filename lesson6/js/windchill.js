397a07350b22334bd8b2d0871183af33565be33e
ilseea <ame08004@byui.edu>
2020-05-30 23:57:02 -0400

    final formula fixed

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,8 +1,11 @@
+let temp = document.getElementById('alto').textContent;
+let wind = document.getElementById('speed').textContent;
+
 document.getElementById("chill").innerHTML =
     "Windchill: " + frio() + "&#8457;";
 
 function frio() {
-    return (Math.round((35.74 + 0.6215 * 46) -
-        (35.75 * (Math.pow(15, 0.16))) +
-        (0.4275 * 46 * (Math.pow(15, 0.16)))));
+    return (Math.round((35.74 + 0.6215 * temp) -
+        (35.75 * (Math.pow(wind, 0.16))) +
+        (0.4275 * temp * (Math.pow(wind, 0.16)))));
 }
