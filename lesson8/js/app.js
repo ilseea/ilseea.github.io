7b08df44b4932b3a17e4f4913c227ea61d636771
ilseea <ame08004@byui.edu>
2020-06-09 01:53:22 -0400

    strmcntr

---
+++ lesson8/js/app.js
@@ -0,0 +1,44 @@
+// let imagesToLoad = document.querySelectorAll('img[data-src]');
+// const loadImages = (image) => {
+//     image.setAttribute('src', image.getAttribute('data-src'));
+//     image.onload = () => {
+//         image.removeAttribute('data-src');
+//     };
+// };
+
+
+
+
+
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+// });
+
+
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((item, observer) => {
+        items.forEach((item) => {
+
+        });
+    }, imgOptions);
+
+} else {
+    preloadImage(entry.target);
+    imgObserver.unobserve(entry.target);
+};
