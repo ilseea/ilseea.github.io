5032166303b8e8a8697a7aca1223fac91f22ec38
ilseea <ame08004@byui.edu>
2020-06-22 15:59:50 -0400

    photos uploaded

--- lesson9/js/trial.js
+++ lesson9/js/trial.js
@@ -11,31 +11,43 @@ fetch(requestURL)
             town.name == 'Fish Haven' || town.name == 'Preston' || town.name == 'Soda Springs'));
 
         result.forEach(town => {
-            let card = document.createElement('section');
-            card.classList.add("article");
-            card.classList.add("card")
+            let towncard = document.createElement('section');
+            towncard.classList.add("towncard");
+
+
+            let towndetail = document.createElement('div');
+            towndetail.classList.add("towndetail")
             let name = document.createElement('h2');
             let slogan = document.createElement('h3');
             let year = document.createElement('p');
             let population = document.createElement('p');
             let rain = document.createElement('p');
-            // let image = document.createElement('img');
+
+            let townpicture = document.createElement('div');
+            townpicture.classList.add("townpicture")
+            let image = document.createElement('img');
 
             name.textContent = town.name;
             slogan.textContent = town.motto;
             year.textContent = 'Year Founded:' + ' ' + town.yearFounded;
             population.textContent = 'Population:' + ' ' + town.currentPopulation;
             rain.textContent = 'Annual Rain Fall:' + ' ' + town.averageRainfall + ' inches';
-            // image.setAttribute('src', town[i].imageurl);
-            // image.setAttribute('alt', town[i].name + ' ' + town[i].lastname + '- ' + towns[i].order);
 
-            card.appendChild(name);
-            card.appendChild(slogan);
-            card.appendChild(year);
-            card.appendChild(population);
-            card.appendChild(rain);
-            // card.appendChild(image);
 
-            document.querySelector('div.cards').appendChild(card);
+            image.setAttribute('src', town.photo);
+            image.setAttribute('alt', town.name);
+
+            towndetail.appendChild(name);
+            towndetail.appendChild(slogan);
+            towndetail.appendChild(year);
+            towndetail.appendChild(population);
+            towndetail.appendChild(rain);
+            towncard.appendChild(towndetail);
+
+            townpicture.appendChild(image);
+            towncard.appendChild(townpicture);
+
+
+            document.querySelector('div.cards').appendChild(towncard);
         });
     });
