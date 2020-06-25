b6d300cd718a080c06ac384526b431fe6ffc2095
ilseea <ame08004@byui.edu>
2020-06-25 19:26:44 -0400

    weather like tutorial

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=1d982ee27d3310460b15a19c65fa132b&units=imperial';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=1d982ee27d3310460b15a19c65fa132b&units=imperial';
 // 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1d982ee27d3310460b15a19c65fa132b'
 
 fetch(apiURL)
@@ -6,16 +6,14 @@ fetch(apiURL)
     .then((jsObject) => {
         console.log(jsObject);
 
-        const currentTemp = document.querySelector('#current-temp');
-        const icon = document.querySelector('img');
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
 
-        currentTemp.textContent = jsObject.main.temp;
 
-        const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`
-        const desc = jsObject.weather[0].description;
-
-        icon.setAttribute('src', imagesrc);
-        icon.setAttribute('alt', desc);
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+        const desc = jsObject.weather[0].description; // note how we reference the weather array
+        document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
 
 
 
