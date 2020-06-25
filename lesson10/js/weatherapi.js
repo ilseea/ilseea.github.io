d79148e1f94cdbfcd6e155d5bce8c941fc8ca8c6
ilseea <ame08004@byui.edu>
2020-06-25 16:50:46 -0400

    api weather test completed

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,10 +1,24 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=1d982ee27d3310460b15a19c65fa132b';
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=1d982ee27d3310460b15a19c65fa132b&units=imperial';
 // 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1d982ee27d3310460b15a19c65fa132b'
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
+
+        const currentTemp = document.querySelector('#current-temp');
+        const icon = document.querySelector('img');
+
+        currentTemp.textContent = jsObject.main.temp;
+
+        const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`
+        const desc = jsObject.weather[0].description;
+
+        icon.setAttribute('src', imagesrc);
+        icon.setAttribute('alt', desc);
+
+
+
     });
 
 // .then(function (jsonObject) {
