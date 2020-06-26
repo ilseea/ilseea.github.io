ae380247ac22b431636cc578463c5680b277e7ae
ilseea <ame08004@byui.edu>
2020-06-26 00:07:20 -0400

    removed extra F

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -2,7 +2,7 @@ let temp = document.getElementById('high-temp').textContent;
 let wind = document.getElementById('wind-speed').textContent;
 
 document.getElementById("windchill").innerHTML =
-    frio() + "&#8457;";
+    frio();
 
 function frio() {
     return (Math.round((35.74 + 0.6215 * temp) -
