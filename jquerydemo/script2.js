5489f01d95440a467ef4a6d76260ae953e2c1bdd
ilseea <ame08004@byui.edu>
2020-06-29 16:26:30 -0400

    events pt1 done

--- jquerydemo/script2.js
+++ jquerydemo/script2.js
@@ -17,13 +17,73 @@
 
 
 
-// toggle on and off ********************
+// toggle on and off event********************
 // $('#btn1').on('click', function () {
 //     $('.para1').toggle();
 // });
 
 
-// double click ********************
-$('#btn1').dblclick(function () {
-    $('.para1').toggle();
+// double click event ********************
+// $('#btn1').dblclick(function () {
+//     $('.para1').toggle();
+// });
+
+// hover event ********************
+// $('#btn1').hover(function () {
+//     $('.para1').toggle();
+// });
+
+// hover isn't an actual event= shorthand ********************
+// $('#btn1').on('hover', function () {
+//     $('.para1').toggle();
+// });
+
+// hover= shorthand for mouseenter/leave********************
+// $('#btn1').on('mouseenter', function () {
+//     $('.para1').toggle();
+// });
+// $('#btn1').on('mouseleave', function () {
+//     $('.para1').toggle();
+// });
+
+// mousemove********************
+// $('#btn1').on('mousemove', function () {
+//     $('.para1').toggle();
+// });
+
+// mousedown/up********************
+// $('#btn1').on('mousedown', function () {
+//     $('.para1').toggle();
+// });
+
+// $('#btn1').on('mouseup', function () {
+//     $('.para1').toggle();
+// });
+
+// hide and show ********************
+$('#btn1').click(function (e) {
+    // console.log(e);
+    // alert(e.currentTarget.id);
+    // alert(e.currentTarget.innerHTML);
+    // alert(e.currentTarget.outerHTML);
+    // alert(e.currentTarget.className);
+});
+
+// mousemove********************
+// $('#btn1').on('mousemove', function (e) {
+//     console.log('Coords: Y: ' + e.clientY + " X: " + e.clientX);
+// });
+
+// document moving********************
+// $(document).on('mousemove', function (e) {
+//     console.log('Coords: Y: ' + e.clientY + " X: " + e.clientX);
+// });
+
+// document moving********************
+// $(document).on('mousemove', function (e) {
+//     $('#coords').append('Coords: Y: ' + e.clientY + " X: " + e.clientX);
+// });
+
+$(document).on('mousemove', function (e) {
+    $('#coords').html('Coords: Y: ' + e.clientY + " X: " + e.clientX);
 });
