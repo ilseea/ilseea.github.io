f3f81cfda53c1378dcc1d5a229eed685baf887cc
ilseea <ame08004@byui.edu>
2020-06-29 18:18:24 -0400

    create 4

---
+++ jquerydemo/js/script4.js
@@ -0,0 +1,61 @@
+// EVENTS
+// button event ********************
+// $('#btn1').click(function () {
+//     alert('Button Clicked!');
+// });
+
+
+
+// input field name turns pink********************
+// $('input').focus(function () {
+//     $('input#name').css('background', 'pink');
+// });
+
+// input field selected turns pink with THIS keyword********************
+// $('input').focus(function () {
+//     // alert(focus)
+//     $(this).css('background', 'pink');
+// });
+
+// input fields blur after unselected********************
+// $('input').blur(function () {
+//     // alert(focus)
+//     $(this).css('background', 'white');
+// });
+
+// input fields********************
+// $('input').keyup(function () {
+//     console.log('keyup');
+// });
+
+// get actual data fields********************
+// $('input').keyup(function (e) {
+//     console.log(e.target.value);
+// });
+
+// input fields********************
+// $('select#gender').change(function () {
+//     alert('changed');
+// });
+
+// alert from button********************
+// $('select#gender').change(function (e) {
+//     alert(e.target.value);
+// });
+
+// input fields********************
+// $('#form').submit(function () {
+//     console.log('Submitted');
+// });
+
+// $('#form').submit(function (e) {
+//     e.preventDefault();
+//     console.log('Submitted');
+// });
+
+
+// $('#form').submit(function (e) {
+//     e.preventDefault();
+//     var name = $('input#name').val();
+//     console.log(name);
+// });
