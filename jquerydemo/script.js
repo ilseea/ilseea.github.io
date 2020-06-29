471253f2b799535f03f08ad1bc6f09f245dc9763
ilseea <ame08004@byui.edu>
2020-06-29 16:02:11 -0400

    notes on 1s and 2e

--- jquerydemo/script.js
+++ jquerydemo/script.js
@@ -1,38 +1,85 @@
-$(document).ready(function () {
-    $("#hidden").hover(function () {
-            $(this).css("color", "black");
-        },
-        function () {
-            $(this).hide()
-        });
-    $("button").click(function () {
-        alert("I'm alerting you");
-    });
-});
-
 // selectors tutorial
+
+// hide ********************
 // $('h1').hide();
-// id selector
+
+// id selectors ********************
 // $('h1#heading1').hide();
-// class selectors
+
+// class selectors ********************
 // $('h1.heading2').hide();
 // $('.heading2').hide();
+
+// change color, take out 'p' to see all span ********************
 // $('p span').css('color', 'red');
-// take out p to see all "span"
 
 
-$('ul#list li:first').css('color', 'blue');
-$('ul#list li:last').css('color', 'green');
-$('ul#list li:even').css('background-color', 'yellow');
-$('ul#list li:odd').css('background-color', '#cccccc');
-$('ul#list li:nth-child(3n)').css('list-style', 'none');
+// select first or last items ********************
+// $('ul#list li:first').css('color', 'blue');
+// $('ul#list li:last').css('color', 'green');
 
+// select even or odd items ********************
+// $('ul#list li:even').css('background-color', 'yellow');
+// $('ul#list li:odd').css('background-color', '#cccccc');
+
+// select specific items ********************
+// $('ul#list li:nth-child(3n)').css('list-style', 'none');
+
+// select other element to hide ********************
 // $(':button').hide();
-$(':text').hide();
+// $(':text').hide();
 
-$('[href]').css('color', 'magenta');
-$('a[href="http://yahoo.com"]').css('color', 'green');
+// select item to style ********************
+// $('[href]').css('color', 'magenta');
+// $('a[href="http://yahoo.com"]').css('color', 'green');
+
+// select all ********************
 // $('*').hide();
 
 //types of selectors you can use:
 //  elements, ids, classes, nest selectors, sudo-selctors
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+//     $("#hidden").hover(function () {
+//             $(this).css("color", "black");
+//         },
+//         function () {
+//             $(this).hide()
+//         });
+//     $("button").click(function () {
+//         alert("I'm alerting you");
+//     });
+// });
--- jquerydemo/script2.js
+++ jquerydemo/script2.js
@@ -1,38 +1,29 @@
-$(document).ready(function () {
-    $("#hidden").hover(function () {
-            $(this).css("color", "black");
-        },
-        function () {
-            $(this).hide()
-        });
-    $("button").click(function () {
-        alert("I'm alerting you");
-    });
+// EVENTS
+// button event ********************
+// $('#btn1').click(function () {
+//     alert('Button Clicked!');
+// });
+
+
+
+// hide and show ********************
+// $('#btn1').on('click', function () {
+//     $('.para1').hide();
+// });
+
+// $('#btn2').on('click', function () {
+//     $('.para1').show();
+// });
+
+
+
+// toggle on and off ********************
+// $('#btn1').on('click', function () {
+//     $('.para1').toggle();
+// });
+
+
+// double click ********************
+$('#btn1').dblclick(function () {
+    $('.para1').toggle();
 });
-
-// selectors tutorial
-// $('h1').hide();
-// id selector
-// $('h1#heading1').hide();
-// class selectors
-// $('h1.heading2').hide();
-// $('.heading2').hide();
-// $('p span').css('color', 'red');
-// take out p to see all "span"
-
-
-$('ul#list li:first').css('color', 'blue');
-$('ul#list li:last').css('color', 'green');
-$('ul#list li:even').css('background-color', 'yellow');
-$('ul#list li:odd').css('background-color', '#cccccc');
-$('ul#list li:nth-child(3n)').css('list-style', 'none');
-
-// $(':button').hide();
-$(':text').hide();
-
-$('[href]').css('color', 'magenta');
-$('a[href="http://yahoo.com"]').css('color', 'green');
-// $('*').hide();
-
-//types of selectors you can use:
-//  elements, ids, classes, nest selectors, sudo-selctors
