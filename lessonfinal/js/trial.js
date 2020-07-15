66d4a682088990dd1d695d84d9364bb41c7a3c0f
ilseea <ame08004@byui.edu>
2020-07-15 16:29:02 -0400

    validate json

--- lessonfinal/js/trial.js
+++ lessonfinal/js/trial.js
@@ -2,6 +2,6 @@ const requestURL = 'https://ilseea.github.io/lessonfinal/data/rent.json';
 
 fetch(requestURL)
     .then(function (response) {
-        // return response.json();
-        console.log(response);
+        return response.json();
+        // console.log(response);
     });
