11cc1a1001a030d710f0e0c7b099cff4f48f3fef
ilseea <ame08004@byui.edu>
2020-07-16 01:28:04 -0400

    table thus far

--- lessonfinal/js/trial.js
+++ lessonfinal/js/trial.js
@@ -12,31 +12,45 @@ fetch(requestURL)
         console.log(jsonObject)
 
         vehicles.forEach(drive => {
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let max = document.createElement('p');
-            let tres = document.createElement('p');
+            let card = document.createElement('tr');
+            let name = document.createElement('td');
+            let head = document.createElement('th');
+            let max = document.createElement('td');
+            let tres = document.createElement('td');
+            let day = document.createElement('td');
+            let half = document.createElement('td');
+            let full = document.createElement('td');
             // let dia = document.createElement('p');
 
             // let image = document.createElement('img');
 
-            // h2.textContent = drive.rentaltype + ' ' + drive.lastrentaltype; (now can add words before `mark below)
+            // name.textContent = drive.rentaltype + ' ' + drive.lastrentaltype; (now can add words before `mark below)
             // if I want to add <strong> use .innerHTML
-            h2.textContent = `${drive.rentaltype}`;
+
+            name.textContent = `${drive.rentaltype}`;
+            head.textContent = 'Seats' + ' ' + 'Reserve';
             max.textContent = `Seats: ${drive.capacity}`;
-            tres.textContent = '3 hr Rental: ' + ' ' + drive.reservation.tres;
+            tres.textContent = 'Reserve: ' + '$' + drive.reservation.tres;
+            day.textContent = 'Full-Day Rental: ' + '$' + drive.reservation.day;
+            half.textContent = '3-Hour Rental: ' + '$' + drive.walkin.half;
+            full.textContent = 'Full-Day Rental: ' + '$' + drive.walkin.full;
             // dia.textContent = 'Day Rental: ' + ' ' + drive.day;
 
             // image.setAttribute('src', drive.imageurl);
             // image.setAttribute('alt', drive.rentaltype + ' ' + drive.lastrentaltype + '- ' + drive.order);
 
-            card.appendChild(h2);
+            card.appendChild(name);
+            card.appendChild(head);
+
             card.appendChild(max);
             card.appendChild(tres);
+            card.appendChild(day);
+            card.appendChild(half);
+            card.appendChild(full);
             // card.appendChild(dia);
 
             // card.appendChild(image);
-            document.querySelector('div.cards').appendChild(card);
+            document.querySelector('table.cards').appendChild(card);
 
         });
     });
