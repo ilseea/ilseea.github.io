558b774847f55cbf2db9c021678939dd4b1e07dd
ilseea <ame08004@byui.edu>
2020-07-18 11:59:17 -0400

    so far

--- lessonfinal/js/prestonapi.js
+++ lessonfinal/js/prestonapi.js
@@ -32,21 +32,3 @@ fetch(forecastURL)
         });
 
     });
-// EVENTS
-
-// const eventURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-// fetch(eventURL)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(jsonObject => {
-//         const townEvents = jsonObject.towns.find(town => {
-//             return town.name == 'Preston';
-//         }).events;
-
-//         townEvents.forEach(event => {
-//             let list = document.createElement('li');
-//             list.innerHTML = event;
-//             document.getElementById('events').appendChild(list);
-//         });
-//     });
